498f47491ff479e245589ed482530d696d8aa164
Terrence <terrence.liu@alumni.ubc.ca>
2023-06-22 22:41:10 -0700

    added timer1.js & initial implementation of timer function

---
+++ timer1.js
@@ -0,0 +1,20 @@
+let input = process.argv.slice(2);
+
+const runTimer = (array) => {
+  // for objects of array
+  for (const time of array) {
+    // if object is a non-negative integer
+    if (Number.isInteger(Number(time)) && Math.sign(Number(time) === 1) ||
+    Number.isInteger(Number(time)) && Math.sign(Number(time) === 0)) {
+      // calls timer
+      setTimer(Number(time));
+    }
+  }
+};
+
+const setTimer = (delay) => {
+  // makes a beeping sound
+  setTimeout(() => process.stdout.write('\x07'), delay * 1000);
+};
+
+runTimer(input);
